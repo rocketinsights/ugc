5f8d2baabe139149a8fa80919b55808c536befbd
Jesse Streb <jesse.streb@gmail.com>
2013-10-18 11:59:24 -0400

    Adding some more goodness

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -1,5 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 var config = require("../configs")[env];
+var s3 = require('../lib/s3').createClient(config.s3);
+var Img = require('../lib/image');
 
 module.exports.getBrandingForUploadWidget = function(req, res) {
   res.send({
@@ -17,10 +19,15 @@ module.exports.getBrandingForUploadWidget = function(req, res) {
 module.exports.uploadVideo = function(req, res) {
   var data = req.body;
   var pathToVideo = data.pathToVide;
+  var ext = req.files.file.name.split('.').pop();
+  var fileName = req.files.file.name;
   
-  if (!pathToVideo) {
-    return res.send(400, {status: 'error', data: "pathToVideo not provided"});
-  }
-  
-  res.send( 200, { status: 'success', data: msg } );
+  var img = new Img(req.files.file.path, ext);
+  img.s3write(s3, req.files.file.path, function(err) {
+    if (err) {
+      return res.send(500, {status: 'error'});
+    }
+    
+    res.send( 200, { status: 'success', data: msg } );
+  });
 };
